fac72ea3765a4d7e50ab73d83f29faa2ef3118cb
Victor Dubois <victor.dubois@free.fr>
2020-07-18 09:41:22 +0200

    first protostrat 2020

--- src/goal/coupe2019.cpp
+++ src/goal/coupe2019.cpp
@@ -109,80 +109,58 @@ Coupe2019::Coupe2019(const bool isYellow, const std::vector<geometry_msgs::Pose>
 }
 
 //Coupe2019::Coupe2019(bool isYellow, Attractor* attractors, unsigned int sizeofAttractors) : StrategieV3(isYellow)
-Coupe2019::Coupe2019(const bool isYellow, const std::vector<geometry_msgs::Point> etapes_as_points) : StrategieV3(isYellow) {
-	//m_attractors = attractors;
-	
+Coupe2019::Coupe2019(const bool isYellow, const std::vector<geometry_msgs::Point> etapes_as_points) : StrategieV3(isYellow) {	
 	//Initialisation des tableaux d'étapes
-	this->numeroEtapeGarage = ETAPE_GARAGE;
-	tableauEtapesTotal = Etape::initTableauEtapeTotal(etapes_as_points.size());//NOMBRE_ETAPES);//new Etape*[NOMBRE_ETAPES];
+
+    //tableauEtapesTotal = Etape::initTableauEtapeTotal(etapes_as_points.size());//NOMBRE_ETAPES);//new Etape*[NOMBRE_ETAPES];
 
 	// Création des étapes
 	// Les étapes correspondant à des actions sont créées automatiquement lors de l'ajout d'actions
 
 	// Initialisation in simulator in initKrabi.cpp
-	//int etapeId = 0;
-	//int start = Etape::makeEtape(positionFromAttractor(0), Etape::DEPART); // départ au fond de la zone de départ
-	//int start = Etape::makeEtape(positionFromPoint(etapes_as_points[++etapeId]), Etape::DEPART); // départ au fond de la zone de départ
-	Position position1 = Position(etapes_as_points[0]);
-	//positionFromPoint(etapes_as_points[0]);
-	std::cout << "posX = " << position1.getX() << std::endl;
-	//Etape::makeEtape(position1, Etape::DEPART); // départ au fond de la zone de départ
-    bool isBlue = false;
-    int start = Etape::makeEtape(Position(0, 0, isBlue), Etape::DEPART); // départ au fond de la zone de départ
-    int pp1 = Etape::makeEtape(Position(0, 200, isBlue), Etape::POINT_PASSAGE); // départ au fond de la zone de départ
-    int pp2 = Etape::makeEtape(Position(200, 200, isBlue), Etape::POINT_PASSAGE); // départ au fond de la zone de départ
-    int goal = Etape::makeEtape(Position(200, 0, isBlue), Etape::ABEILLE); // départ au fond de la zone de départ
-	Etape::get(pp1)->addVoisins(start);
-	Etape::get(pp1)->addVoisins(pp2);
-	Etape::get(pp2)->addVoisins(goal);
-	//Etape::get(pp2)->addVoisins(start);
+    //Initialisation des tableaux d'étapes
+    this->numeroEtapeGarage = ETAPE_GARAGE;
+    tableauEtapesTotal = Etape::initTableauEtapeTotal(NOMBRE_ETAPES);//new Etape*[NOMBRE_ETAPES];
 
-/*
-	// COLORS
-	int mid_red = Etape::makeEtape(Position(0,0, true), Etape::DEPART);
-	int attractorId = 0;
-	int out_of_red = Etape::makeEtape(positionFromAttractor(attractorId++));
-	Etape::get(mid_red)->addVoisins(out_of_red);// out of start
-	
-	int out_of_green = Etape::makeEtape(positionFromAttractor(attractorId++));
-	int mid_green = Etape::makeEtape(new Abeille(positionFromAttractor(attractorId++)));
-	Etape::get(mid_green)->addVoisins(out_of_green);// push greenium
-	Etape::get(out_of_red)->addVoisins(out_of_green);// From red to green
-	
-	//int mid_blue = Etape::makeEtape(new Abeille(positionFromAttractor(attractorId++)));
-	//int out_of_blue = Etape::makeEtape(positionFromAttractor(attractorId++));
-	//Etape::get(mid_blue)->addVoisins(out_of_blue);// push blue
-	//Etape::get(out_of_blue)->addVoisins(out_of_green);// from green to blue
-	
-	// ACCELERATOR
-	int out_of_top_of_accel = Etape::makeEtape(positionFromAttractor(attractorId++));
-	int in_front_of_top_of_accel = Etape::makeEtape(new Accelerator(positionFromAttractor(attractorId++)));
-	Etape::get(in_front_of_top_of_accel)->addVoisins(out_of_top_of_accel);// accel
-	Etape::get(out_of_red)->addVoisins(out_of_top_of_accel);// red to accel
-	
-	// GOLDENIUM
-	int out_of_goldenium = Etape::makeEtape(positionFromAttractor(attractorId++));
-	int in_front_of_goldenium = Etape::makeEtape(new Goldenium(positionFromAttractor(attractorId++)));
-	Etape::get(out_of_goldenium)->addVoisins(in_front_of_goldenium);// goldenium
-	Etape::get(out_of_top_of_accel)->addVoisins(out_of_goldenium);// goldenium to accel
-*/	
-	/** Points de passage **/
-/*	int waypoint_behind_backhole = Etape::makeEtape(positionFromAttractor(attractorId++));
-	Etape::get(out_of_green)->addVoisins(waypoint_behind_backhole);// green to waypoint
-	Etape::get(out_of_red)->addVoisins(waypoint_behind_backhole);// red to waypoint
-	Etape::get(out_of_top_of_accel)->addVoisins(waypoint_behind_backhole);// accel to waypoint
-	
-	//int second_waypoint_behind_backhole = Etape::makeEtape(positionFromAttractor(attractorId++));
-	//Etape::get(out_of_top_of_accel)->addVoisins(second_waypoint_behind_backhole);// accel to 2waypoint
-	//Etape::get(waypoint_behind_backhole)->addVoisins(second_waypoint_behind_backhole);// waypoint to 2waypoint
+    // Création des étapes
+    // Les étapes correspondant à des actions sont créées automatiquement lors de l'ajout d'actions
 
-	// Reservoir eau proche
-	//int reservoirProche = Etape::makeEtape(new ReservoirEau(Position(220, 840, true)));
+    // Initialisation in simulator in initKrabi.cpp
+    int main_port = Etape::makeEtape(Position(200, 800, true), Etape::DEPART); // départ au fond de la zone de départ
 
-	for (auto i = 0; i < attractorId; i++) {
-		positionFromAttractor(i);
-	}
-*/
+    int lighthouse = Etape::makeEtape(Position(301,  200, true));
+
+    int out_of_lighthouse = Etape::makeEtape(Position(640, 400, true));
+
+    int out_of_main_port = Etape::makeEtape(Position(700, 800, true));
+
+    Etape::get(main_port)->addVoisins(out_of_main_port);
+    Etape::get(lighthouse)->addVoisins(out_of_lighthouse);
+    Etape::get(out_of_main_port)->addVoisins(out_of_lighthouse);
+
+    int first_air = Etape::makeEtape(new Abeille(Position(230, 1800, true)));
+    int second_air = Etape::makeEtape(new Abeille(Position(635, 1800, true)));
+    int out_of_air = Etape::makeEtape(Position(430, 1400, true));
+
+    Etape::get(first_air)->addVoisins(out_of_air);
+    Etape::get(second_air)->addVoisins(out_of_air);
+
+    int south = Etape::makeEtape(new Abeille(Position(150, 1250, true)));
+    int north = Etape::makeEtape(new Abeille(Position(150, 320, true)));
+
+    Etape::get(south)->addVoisins(out_of_air);
+    Etape::get(north)->addVoisins(out_of_lighthouse);
+    Etape::get(north)->addVoisins(lighthouse);
+
+
+    /** Points de passage **/
+    int waypoint_south = Etape::makeEtape(Position(640, 1300, true));
+    int waypoint_out_of_enemy_port = Etape::makeEtape(Position(1100, 1400, true));
+    Etape::get(out_of_air)->addVoisins(waypoint_south);// green to waypoint
+    Etape::get(out_of_main_port)->addVoisins(waypoint_south);// red to waypoint
+    Etape::get(waypoint_out_of_enemy_port)->addVoisins(waypoint_south);// red to waypoint
+
+
 #ifdef QTGUI
 	qDebug() << Etape::getTotalEtapes();
 #endif
