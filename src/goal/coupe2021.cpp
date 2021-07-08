d3f5fda64a64b404fef7645ed271c8059b3ca0a4
Victor Dubois <victor.dubois@navya.tech>
2021-07-08 15:39:18 +0200

    increase time for mouillage

--- src/goal/coupe2021.cpp
+++ src/goal/coupe2021.cpp
@@ -316,14 +316,14 @@ int Coupe2021::getScoreEtape(int i)
         break;
     case Etape::MOUILLAGE_NORD:
         l_score = 0;
-        if (m_good_mouillage == Etape::MOUILLAGE_NORD && getRemainingTime() < 30.f)
+        if (m_good_mouillage == Etape::MOUILLAGE_NORD && getRemainingTime() < 45.f)
         {
             l_score = 200;
         }
         break;
     case Etape::MOUILLAGE_SUD:
         l_score = 0;
-        if (m_good_mouillage == Etape::MOUILLAGE_SUD && getRemainingTime() < 30.f)
+        if (m_good_mouillage == Etape::MOUILLAGE_SUD && getRemainingTime() < 45.f)
         {
             l_score = 200;
         }
