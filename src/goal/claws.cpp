c034a04ae6ff565b51bc9518d6709fbe3ee03474
Maxou <maxhol@hotmail.fr>
2024-05-09 11:11:19 +0200

    close claws better v1

--- src/goal/claws.cpp
+++ src/goal/claws.cpp
@@ -36,14 +36,14 @@ Angle Claws::getAngle()
 
 void Claws::grab_pile(bool a_do_sleep)
 {
-    m_right_servo->set(130, 100);
+    m_right_servo->set(150, 100);
 
     // Pour ne pas s'emmeler les pinces
     if (m_left_servo->getAngle() < 70)
     {
         conditionnal_sleep(1e6, a_do_sleep);
     }
-    m_left_servo->set(80, 100);
+    m_left_servo->set(60, 100);
     conditionnal_sleep(1.5e6, a_do_sleep);
 }
 
