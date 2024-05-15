2e75e0aea8ae75e9d22221eb48d42d6dae2be57b
Victor on ROS2 VM <victor.dubois@free.fr>
2024-05-15 22:13:13 +0200

    fix dijkstra => score was computed with distances in mm

--- src/krabilib/strategie/strategiev3.cpp
+++ src/krabilib/strategie/strategiev3.cpp
@@ -263,7 +263,7 @@ int StrategieV3::update()
                 //        score =
                 //        modificateurTemporel*(10000-m_tableau_etapes_total[i]->getDistance() +
                 //        scoreTypeEtape);
-                score = (10000 - m_tableau_etapes_total[i]->getDistance() + scoreTypeEtape);
+                score = (100000 - m_tableau_etapes_total[i]->getDistance()*1000 + scoreTypeEtape*1000);
                 if ((scoreMaxi < score) && scoreTypeEtape
                     && (m_tableau_etapes_total[i]->getDistance() != -1))
                 {
