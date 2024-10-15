ba684e71a3fb892b42c209bb3e7f1e6022058095
Victor Dubois <victor.dubois@free.fr>
2024-10-15 21:51:50 +0200

    fix for jazzy

--- src/krabilib/position.cpp
+++ src/krabilib/position.cpp
@@ -4,7 +4,7 @@
 #include "tf2/LinearMath/Transform.h"
 #include "tf2/LinearMath/Quaternion.h"
 #include "tf2/transform_datatypes.h"
-#include "tf2_geometry_msgs/tf2_geometry_msgs.h"
+#include "tf2_geometry_msgs/tf2_geometry_msgs.hpp"
 
 // Constructeur par défaut avec des coordonnées nulles.
 Position::Position(Distance x, Distance y)
