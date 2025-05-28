7dde601a1b9c3e5af688705023c4a60c61724639
Victor Dubois <victor.dubois@navya.tech>
2025-05-28 10:53:06 +0200

    clang_format

--- include/krabilib/strategie/dijkstra.h
+++ include/krabilib/strategie/dijkstra.h
@@ -1,66 +1,70 @@
-//Prononcer Da_ik_stra
-//http://fr.wikipedia.org/wiki/Algorithme_de_Dijkstra
+// Prononcer Da_ik_stra
+// http://fr.wikipedia.org/wiki/Algorithme_de_Dijkstra
 
 #ifndef DIJKSTRA_H
 #define DIJKSTRA_H
 
 #include "krabilib/strategie/etape.h"
 
-
 class Dijkstra
 {
 public:
     /** @brief Constructeur de Dijkstra *
-    * @param tableauEtapes le tableau contenant toutes les étapes du graphe à parcourir *
-    * @param nbEtapes le nombre d'étapes contenues dans le tableau */
+     * @param tableauEtapes le tableau contenant toutes les étapes du graphe à parcourir *
+     * @param nbEtapes le nombre d'étapes contenues dans le tableau */
     Dijkstra(std::vector<Etape*>& tableauEtapes, int nbEtapes);
 
     /** @brief Renvoi un pointeur vers l'étape courante *
-    * @return pointeur vers l'étape courante */
+     * @return pointeur vers l'étape courante */
     Etape* getEtapeCourante();
 
     /** @brief Calcul et renvoi la distance à vol d'oiseau entre deux étapes *
-    * @param etapeDepart l'étape de départ *
-    * @param etapeArrivee l'étape d'arrivée *
-    * @return distance  à vol d'oiseau entre deux étapes*/
+     * @param etapeDepart l'étape de départ *
+     * @param etapeArrivee l'étape d'arrivée *
+     * @return distance  à vol d'oiseau entre deux étapes*/
     static float calculDistanceDirect(Etape* etapeDepart, Etape* etapeArrivee);
 
     /** @brief Set l'étape courante *
-    * @param numeroEtapeCourante le numéro de l'étape courante */
+     * @param numeroEtapeCourante le numéro de l'étape courante */
     void setEtapeCourante(int numeroEtapeCourante);
 
-    /** @brief Fonction principale, applique l'algorithme de Dijkstra à toutes les étapes du tableau, *
-    * en mettant à jour leur distance par rapport à l'étape courante *
-    * @return 0 si tout va bien, -1 si l'étape en cours a changée (ce qui arrive pour décoincer le robot) */
+    /** @brief Fonction principale, applique l'algorithme de Dijkstra à toutes les étapes du
+     * tableau, * en mettant à jour leur distance par rapport à l'étape courante *
+     * @return 0 si tout va bien, -1 si l'étape en cours a changée (ce qui arrive pour décoincer le
+     * robot) */
     int run();
 
     /** @brief Décomposition de la fonction run, pour ne faire qu'une seule étape à la fois,
-    * et ainsi tenir dans un temps de traitement de 5 ms *
-    * @param step le numéro du miniRun *
-    * @return -1 en cas de graphe bloqué */
+     * et ainsi tenir dans un temps de traitement de 5 ms *
+     * @param step le numéro du miniRun *
+     * @return -1 en cas de graphe bloqué */
     int miniRun(int step);
 
-    /** @brief Renvoi le numéro de l'étape la plus proche dont on n'a pas encore mis à jour les voisins *
-    * @param classementEtapeMinimale sert à indiquer le classement de l'étape trouvée (de la plus proche à la plus éloignée de l'étape origine *
-    * @return le numéro de l'étape la plus proche dont on n'a pas encore mis à jour les voisins */
+    /** @brief Renvoi le numéro de l'étape la plus proche dont on n'a pas encore mis à jour les
+     * voisins *
+     * @param classementEtapeMinimale sert à indiquer le classement de l'étape trouvée (de la plus
+     * proche à la plus éloignée de l'étape origine *
+     * @return le numéro de l'étape la plus proche dont on n'a pas encore mis à jour les voisins */
     int trouverMin(int classementEtapeMinimale);
 
-    /** @brief Met à jour les voisin de l'étape spécifiée. Cela correspond à mettre à jour leur distance à l'origine si elle n'existait pas, ou bien si passer par cette étape est plus court que le chemin envisagé jusqu'alors *
-    * @param numeroEtape le numéro de l'étape dont il faut mettre à jour les voisins */
+    /** @brief Met à jour les voisin de l'étape spécifiée. Cela correspond à mettre à jour leur
+     * distance à l'origine si elle n'existait pas, ou bien si passer par cette étape est plus court
+     * que le chemin envisagé jusqu'alors *
+     * @param numeroEtape le numéro de l'étape dont il faut mettre à jour les voisins */
     void mettreAJourVoisins(int numeroEtape);
 
     /** @brief Renvoi la distance de l'étape spécifiée à l'étape origine *
-    * @param numeroEtape le numéro de l'étape dont on veut connaître la distance *
-    * @return la distance de l'étape spécifiée à l'étape origine */
+     * @param numeroEtape le numéro de l'étape dont on veut connaître la distance *
+     * @return la distance de l'étape spécifiée à l'étape origine */
     float getDistance(int numeroEtape);
 
     /** @brief Renvoi la distance de l'étape spécifiée à l'étape origine *
-    * @param etape l'étape dont on veut connaître la distance *
-    * @return la distance de l'étape spécifiée à l'étape origine */
+     * @param etape l'étape dont on veut connaître la distance *
+     * @return la distance de l'étape spécifiée à l'étape origine */
     float getDistance(Etape* etape);
 
     /** @brief Renvoi le nombre d'etapes constituant de graphe *
-    * @return le nombre d'etapes constituant de graphe */
+     * @return le nombre d'etapes constituant de graphe */
     int getNombreEtapes();
 
     /** @brief Reset l'index de progression pour miniRun */
