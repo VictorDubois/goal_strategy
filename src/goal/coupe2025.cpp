3e9806ba81074f7a2d7bfaca2d65a8ab2096a784
Victor on rpi4 24.04 <victor.dubois@free.fr>
2025-05-29 00:00:47 +0200

    fix backstage position

--- src/goal/coupe2025.cpp
+++ src/goal/coupe2025.cpp
@@ -93,7 +93,7 @@ Coupe2025::Coupe2025(const bool isYellow, const StartingPosition2025 starting_po
     int zone_contruction_front           = Etape::makeEtape(new AireDeConstruction(Pose(positionC(-0.275f, 0.775f), Angle(0.0f)), Owner::us, AireSize::AIRE_BIG));
     int zone_contruction_cote            = Etape::makeEtape(new AireDeConstruction(Pose(positionC(1.275f, 0.125f), Angle(0.0f)), Owner::us, AireSize::AIRE_BIG));
     int zone_contruction_backstage = Etape::makeEtape(new AireDeConstruction(
-      Pose(positionC(-1.0f, -0.7f), Angle(0.0f)), 
+      Pose(positionC(-1.1f, -0.480f), Angle(0.0f)), 
 	Pose(positionC(-1.125f, -0.775f), Angle(0.0f)),
       Owner::us,
       AireSize::AIRE_BIG));
