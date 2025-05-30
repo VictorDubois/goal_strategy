e72a5eb0d9dcda00e74f2d975085e702c1037f48
Victor Dubois <victor.dubois@navya.tech>
2025-05-30 17:29:31 +0200

    fix StepperModes (+ clangformat)

--- include/goal_strategy/stepper.h
+++ include/goal_strategy/stepper.h
@@ -1,13 +1,13 @@
 #pragma once
-#include "rclcpp/rclcpp.hpp"
 #include "krabi_msgs/msg/infos_stepper.hpp"
+#include "rclcpp/rclcpp.hpp"
 
 enum StepperMode
 {
-    DISABLE,
-    POSITION,
-    SPEED,
-    HOMING
+    DISABLE = 0,
+    POSITION = 10,
+    SPEED = 20,
+    HOMING = 30
 };
 
 class StepperMotor
@@ -15,48 +15,75 @@ class StepperMotor
 protected:
     StepperMode m_stepper_mode;
     int16_t m_target_position; // mm
-    int16_t m_distance_to_go; // mm
+    int16_t m_distance_to_go;  // mm
     void updateInfos(krabi_msgs::msg::InfosStepper a_new_infos)
     {
         m_distance_to_go = a_new_infos.distance_to_go;
-    };  
+    };
 
 private:
-    uint16_t m_speed; // mm/s
-    uint16_t m_max_accel; // mm/s²
-    uint16_t m_max_current;// mA
+    uint16_t m_speed;       // mm/s
+    uint16_t m_max_accel;   // mm/s²
+    uint16_t m_max_current; // mA
 
 public:
-    StepperMotor(uint16_t speed, uint16_t accel, uint16_t max_current): m_speed(speed), m_max_accel(accel), m_max_current(max_current)
+    StepperMotor(uint16_t speed, uint16_t accel, uint16_t max_current)
+      : m_speed(speed)
+      , m_max_accel(accel)
+      , m_max_current(max_current)
     {
         m_stepper_mode = StepperMode::DISABLE;
         m_target_position = 0;
     };
 
-    explicit  StepperMotor()
+    explicit StepperMotor()
       : StepperMotor(100, 2000, 5000)
     {
     }
-    //void publish(float a_speed, float a_angle);
+    // void publish(float a_speed, float a_angle);
 
-    void setSpeed(uint16_t a_speed) {m_speed = a_speed;};
-    void setEnable(StepperMode a_new_mode){m_stepper_mode = a_new_mode;};
+    void setSpeed(uint16_t a_speed)
+    {
+        m_speed = a_speed;
+    };
+    void setEnable(StepperMode a_new_mode)
+    {
+        m_stepper_mode = a_new_mode;
+    };
 
-    uint16_t getAccel() {return m_max_accel;};
-    uint16_t getSpeed() {return m_speed;};
-    uint16_t getMaxCurrent() {return m_max_current;};
-    uint16_t getTargetPosition() {return m_target_position;};
-    StepperMode getStepperMode() {return m_stepper_mode;};
-    bool movmentDone() { return m_distance_to_go == 0;};
+    uint16_t getAccel()
+    {
+        return m_max_accel;
+    };
+    uint16_t getSpeed()
+    {
+        return m_speed;
+    };
+    uint16_t getMaxCurrent()
+    {
+        return m_max_current;
+    };
+    uint16_t getTargetPosition()
+    {
+        return m_target_position;
+    };
+    StepperMode getStepperMode()
+    {
+        return m_stepper_mode;
+    };
+    bool movmentDone()
+    {
+        return m_distance_to_go == 0;
+    };
 };
 
-class StepperElevator: public StepperMotor
+class StepperElevator : public StepperMotor
 {
 private:
-  //float m_;
-  bool m_homing_done;
-  uint8_t m_homing_sequences_done;
-  uint16_t m_max_height_mm;
+    // float m_;
+    bool m_homing_done;
+    uint8_t m_homing_sequences_done;
+    uint16_t m_max_height_mm;
 
 public:
     void updateElevatorInfos(krabi_msgs::msg::InfosStepper a_new_infos)
@@ -64,14 +91,26 @@ public:
         m_homing_done = a_new_infos.homing_switch_on;
         m_homing_sequences_done = a_new_infos.homing_sequences_done;
         updateInfos(a_new_infos);
-    };  
-    void doHoming(){m_stepper_mode = StepperMode::HOMING;};
-    bool homingDone(){return m_homing_done;};
+    };
+    void doHoming()
+    {
+        m_stepper_mode = StepperMode::HOMING;
+    };
+    bool homingDone()
+    {
+        return m_homing_done;
+    };
     bool goToPosition(int16_t a_distance_in_mm);
 
-    StepperElevator(uint16_t a_max_height_mm) : StepperMotor()
-    {m_max_height_mm = a_max_height_mm;};
+    StepperElevator(uint16_t a_max_height_mm)
+      : StepperMotor()
+    {
+        m_max_height_mm = a_max_height_mm;
+    };
 
-    StepperElevator(uint16_t speed, uint16_t accel, uint16_t max_current, uint16_t a_max_height_mm) : StepperMotor(speed, accel, max_current)
-    {m_max_height_mm = a_max_height_mm;};
+    StepperElevator(uint16_t speed, uint16_t accel, uint16_t max_current, uint16_t a_max_height_mm)
+      : StepperMotor(speed, accel, max_current)
+    {
+        m_max_height_mm = a_max_height_mm;
+    };
 };
